de803d68ea0dc09e2e140a0133dc8bc86810fe32
kkchans <81597195+kkchans@users.noreply.github.com>
2022-07-28 15:15:25 +0900

    김찬치

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  let [글제목,글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬독학']); 
   let posts = '집에 가고 싶다';
   return (
     <div className="App">
       <div className="black-nav">
           <div style ={{color : 'blue', fontSize : '30px'}}>개발 BLOG</div>
       </div>
+      <div className="list">
+        <h4> { 글제목[0] } </h4>
+        <p>내용을 씁시다</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h4> { 글제목[1] } </h4>
+        <p>내용을 씁시다</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h4> { 글제목[2] } </h4>
+        <p>내용을 씁시다!</p>
+        <hr/>
+      </div>
       <img src={logo} />
       <h4>{posts}</h4>
-    </div>
+    </div> 
   );
 }
 
