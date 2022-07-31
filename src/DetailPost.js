1cfad817e194e86daa4da6c548a359427a498f41
mina kim <81088157+kimmina888@users.noreply.github.com>
2022-07-31 22:57:14 +0900

    댓글 기능 추가

--- src/DetailPost.js
+++ src/DetailPost.js
@@ -1,13 +1,61 @@
-import React from "react";
-
+import React, { useState } from "react";
 const DetailPost = (props) =>{
+  let [userName] = useState('익명');
+  let [comment, setComment] = useState('');
+  let [feedComments, setFeedComments] = useState([]);
+  let [isValid, setIsValid] = useState(false);
   const {title, date, content } = props;
-
+  let post = e => {
+    const copyFeedComments = [...feedComments];
+    copyFeedComments.push(comment);
+    setFeedComments(copyFeedComments);
+    setComment('');
+  }
+  
+  const CommentList = props => {
+    return (
+      <div className="userCommentBox">
+        <p className="userName">{props.userName}</p>
+        <div className="userComment">{props.userComment}</div>
+      </div>
+    )
+   }
     return (
         <div className="modal">
           <h2>{title}</h2>
           <p>{date}</p>
           <p>{content}</p>
+          <div className="Comments">
+              <input type="text" className="inputComment" placeholder="댓글 달기.."
+              onChange={e => {
+               setComment(e.target.value);
+              }}
+              onKeyUp = { e => {
+                  e.target.value.length > 0?
+                  setIsValid(true):
+                  setIsValid(false);
+                }
+              }
+              value = {comment}
+            />
+            <button
+            type="button"
+            className={
+              comment.length > 0? 'submitCommentActive':
+              'submitCommentInactive'
+            }
+            onClick={post} disabled={isValid? false : true}
+            >게시</button>
+             {feedComments.map((commentArr, i) => {
+              return (
+                <CommentList
+                  userName = {userName}
+                  userComment = {commentArr}
+                  key = {i}
+                />
+              );
+           })}
+          </div>
       </div>
     );
 }
--- src/Post.js
+++ src/Post.js
@@ -1,9 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 
 function Post(props) {
+  let [Love, LoveChange] = useState(0);
     return (
       <div className="list">
-        <h4> {props.title} </h4>
+      <h4> {props.title} <span onClick={() => {LoveChange(Love + 1)} }> ♥</span> {Love} </h4>
         <p>{props.content}</p>
         <hr />
       </div>
